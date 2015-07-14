2093063f51e237695c7e4cfbb10fb3ff50ebdfb8
IanConery <ianconery@gmail.com>
2015-07-13 21:13:45 -0700

    backbone troubles

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -19,41 +19,11 @@ app.init = function() {
   // app.fetch();
   var messages = new Messages();
   var messagesView = new MessageBoard({model: messages}); 
-  $('.bottom').prepend(this.messagesView);
-  console.log(this.messagesView)
+
+  var html = messagesView.render();
+  // console.log($);
+  $('body').append('<p>test</p>');
 }
-
-// app.fetch = function() {
-//     var data = $.ajax({
-//     url: 'https://api.parse.com/1/classes/chatterbox',
-//     type: 'GET',
-//     contentType: 'application/json',
-//     dataType: 'JSON',
-//     success: function (data) {
-//       console.log('chatterbox: OK ');
-//       console.log(data.results);
-
-//       var messages = new Messages(data.results);
-//       var messagesView = new MessageBoard({model: messages});    
-//     }
-//   });
-// }
-
-// app.send = function(message) {
-//   $.ajax({
-//     // This is the url you should use to communicate with the parse API server.
-//     url: 'https://api.parse.com/1/classes/chatterbox',
-//     type: 'POST',
-//     data: JSON.stringify(message),
-//     contentType: 'application/json',
-//     success: function (data) {
-//       console.log('chatterbox: Message sent');
-//     },
-//     error: function (data) {
-//       console.error('chatterbox: Failed to send message');
-//     }
-//   });
-// }
 
 
 /* BACKBONE */
@@ -81,53 +51,34 @@ var Messages = Backbone.Collection.extend({
 var MessageBoard = Backbone.View.extend({
   // keeps track of the changes
   initialize: function(){
+    _.bindAll(this, 'render');
     var that = this;
     this.collection = new Messages();
-    var data = this.collection.fetch({
-        success: function(items){
-          _.each(items.toJSON(), function (item){
-            // console.log(item);
-            that.render(item);
 
-          })
-        }
-    });
-
-    // console.log(this.collection.fetch());
-
-    // json 
-    // _.each(data) { item}
-    // return JSON.stringify(model)
-
-    //  on success, each loop to return render for each object
-
-    // this.model.on('change', function(){
-    //   this.render()
-    // }, this)
+    // this is ugly, unfortunately change or sync doesn't seem to work as they should
+    setInterval(function(){
+      that.collection.fetch({
+          success: function(items){
+            _.each(items.toJSON(), function (item){
+              // console.log(item);
+              that.render(item);
+            })
+          }
+      });
+    }, 2000)
   },
   // runs when the values are changed and prepends it to our html
   render: function(item){
     // console.log(this.model.get('text'));
     // console.log('rendering');
     // return html .. 
-    console.log(item);
-    return _.template('hello');
-    
+    // console.log(item);
+    // if (item) 
+    console.log(item.username, item.text, item.chatroom);
   }
 });
 
-
-
 app.init();
-
-
-
-
-
-
-
-//$('body').append(messagesView.render());
-
 
 
 // var router = Backbone.Router.extend({
