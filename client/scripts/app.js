c0db4623c79817aa60c4384c142798f8163500b6
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-07-14 17:11:02 -0700

    backbone implementation mostly complete

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,34 +3,155 @@ var app = {}
 // initialize the app
 app.init = function() {
   var messages = new Messages();
-  var messagesView = new MessageBoard({model: messages}); 
+  messages.retrieveMsgs();
+  var messagesView = new MessagesView({
+    el: $('.list'),
+    collection: messages
+  });
+
+  var formView = new FormView({
+    el: $('#form'),
+    // formView needs to have access to the collection to pass new messages directly
+    collection: messages
+  })
 }
+
+/* BACKBONE */
+// model
+var Message = Backbone.Model.extend({
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  defaults: {
+    username: '',
+    text: ''
+  }
+});
+
+// collection of model
+var Messages = Backbone.Collection.extend({
+  model: Message,
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  retrieveMsgs: function() {
+    this.fetch({
+      // fetch data ordered by creation date.. 
+      data: {
+        order: '-createdAt'
+      }
+    });
+  },
+  parse: function(response, options) {
+    var results = [];
+    // reverse array to display newest messages on top
+    for (var i = response.results.length - 1; i >= 0; i--) {
+      results.push(response.results[i]);
+    };
+    return results;
+  }
+});
+
+// View of sindle message / model
+
+var MessageView = Backbone.View.extend({
+  render: function() {
+    var item = this.model.attributes;
+    if (item !== undefined) {
+
+      // sanitize values
+      for (var key in item) {
+        // make sure item[key] is a truthy value (no null, undefined, etc)
+        if (item.hasOwnProperty(key) && item[key]) {
+          item[key] = sanitize(item[key]);
+          // item[key] = removeTags(item[key]);
+        }
+      }
+
+      // set interpolate for handlebar templating settings via underscore
+      _.templateSettings = {
+        interpolate: /\{\{(.+?)\}\}/g
+      };
+
+      // define underscore template
+      var template = _.template('<div class="username">{{user}}</div><div class="message">{{message}}</div>');
+
+      // create output with underscore template & passed values
+      var html = template({
+        user: item.username,
+        message: item.text
+      });
+
+      return this.$el.html(html);
+    }
+  }
+})
+
+// view of collection
+var MessagesView = Backbone.View.extend({
+  initialize: function() {
+    this.collection.on('sync', this.render, this);
+  },
+  // runs when the values are changed and prepends it to our html
+  render: function() {
+    this.collection.forEach(this.renderMessage, this)
+  },
+
+  renderMessage: function(message) {
+    var messageView = new MessageView({
+      // el: $('.list'),
+      model: message
+    });
+    var html = messageView.render();
+
+    // console.log(html);
+    this.$el.prepend(html);
+  }
+});
+
+var FormView = Backbone.View.extend({
+
+  events: {
+    'submit #send': 'executeSubmit'
+  },
+
+  executeSubmit: function(e) {
+    e.preventDefault();
+    var message = {
+      text: $('#message').val(),
+      username: 'Grinding HR Student'
+    }
+
+    //var sendmessage = new Message(message);
+    // sendmessage.save();
+
+    // better, because immediately tied to the collection object:
+    this.collection.create(message);
+  }
+})
+
+// start app by calling initialize function
+app.init();
+
+
 
 // various sanitizing functions, because people are assholes
 var sanitize = function(input) {
-    var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
-           // replace(/window/gi, '').
-           replace(/<[\/\!]*?[^<>]*?>/gi, '').
-           replace(/<style[^>]*?>.*?<\/style>/gi, '').
-           replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
-      return output;
-  };
+  var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
+  replace(/<[\/\!]*?[^<>]*?>/gi, '').
+  replace(/<style[^>]*?>.*?<\/style>/gi, '').
+  replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
+  return output;
+};
 
 
 var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
 
 var tagOrComment = new RegExp(
-    '<(?:'
-    // Comment body.
-    + '!--(?:(?:-*[^->])*--+|-?)'
-    // Special "raw text" elements whose content should be elided.
-    + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
-    + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
-    // Regular name
-    + '|/?[a-z]'
-    + tagBody
-    + ')>',
-    'gi');
+  '<(?:'
+  // Comment body.
+  + '!--(?:(?:-*[^->])*--+|-?)'
+  // Special "raw text" elements whose content should be elided.
+  + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*' + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+  // Regular name
+  + '|/?[a-z]' + tagBody + ')>',
+  'gi');
 
 function removeTags(html) {
   var oldHtml;
@@ -40,88 +161,3 @@ function removeTags(html) {
   } while (html !== oldHtml);
   return html.replace(/</g, '&lt;');
 }
-
-/* BACKBONE */
-// model
-var Message = Backbone.Model.extend({
-});
-
-// collection of model
-var Messages = Backbone.Collection.extend({
-  model: Message,
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  parse: function(response){
-    return response.results
-  },
-  // we need to override Backbone.sync to escape Same Origin Policy
-  sync: function(method, model, options){
-    var that = this;
-    var params = _.extend({
-      type: 'GET',
-      dataType: 'JSON',
-      url: that.url
-    }, options);
-    return $.ajax(params)
-  }
-});
-// view of collection
-var MessageBoard = Backbone.View.extend({
-  // keeps track of the changes
-  initialize: function(){
-    _.bindAll(this, 'render');
-
-    // need to refer to this inside each loop 
-    var that = this;
-    // build colection 
-    this.collection = new Messages();
-
-    // fetch data from server via API and pass results when successful
-    this.collection.fetch({
-        success: function(items){
-          _.each(items.toJSON(), function (item){
-            that.render(item);
-          })
-        }
-    });
-  },
-  // runs when the values are changed and prepends it to our html
-  render: function(item){
-    if (item !== undefined) {
-
-      // set defaults for values
-      item.username = item.username || 'annonomyous'
-      item.text = item.text || 'No message'
-      item.roomname = item.roomname || 'No Roomname'
-
-      // sanitize values
-      for (var key in item) {
-        item[key] = sanitize(item[key]);
-        item[key] = removeTags(item[key]); 
-      }
-
-      // set interpolate for handlebar templating settings via underscore
-      _.templateSettings = {
-        interpolate: /\{\{(.+?)\}\}/g
-      };
-
-      // define underscore template
-      var template = _.template('<li><div class="username">{{user}}</div><div class="message">{{message}}</div></li>'); 
-
-      // create output with underscore template & passed values
-      var html = template({
-        user: item.username,
-        message: item.text
-      });
-
-      // prepend output html to page
-      $('.list').prepend(html);
-    }
-  }
-});
-
-// start app by calling initialize function
-app.init();
-
-
-
-
